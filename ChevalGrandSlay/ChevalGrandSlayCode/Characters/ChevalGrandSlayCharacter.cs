ec3a8228a7a0d3b9730058de2e7a0dd34b1eb8ee
Fitswc <ericchufengzeng@163.com>
2026-09-03 00:10:12 +0800

    Adjust some character related files

--- ChevalGrandSlay/ChevalGrandSlayCode/Characters/ChevalGrandSlayCharacter.cs
+++ ChevalGrandSlay/ChevalGrandSlayCode/Characters/ChevalGrandSlayCharacter.cs
@@ -31,7 +31,7 @@ public sealed class ChevalGrandSlayCharacter : ModCharacterTemplate<ChevalGrandS
     public override Color MapDrawingColor => ThemeColor;
 
     // 人物性别（男女中立）。
-    public override CharacterGender Gender => CharacterGender.Neutral;
+    public override CharacterGender Gender => CharacterGender.Feminine;
 
     // 初始血量和金币。
     public override int StartingHp => 75;
